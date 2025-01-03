508001d1928c179945156cbd02da6a0ec4d3c3c0
Ujjwal Arora <ukarora9@gmail.com>
2025-01-04 01:39:28 +0530

    Initial Commit

---
+++ RealmAzamsharpCRUD/RealmAzamsharpCRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmAzamsharpCRUDApp.swift
+//  RealmAzamsharpCRUD
+//
+//  Created by Ujjwal Arora on 04/01/25.
+//
+
+import SwiftUI
+
+@main
+struct RealmAzamsharpCRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
